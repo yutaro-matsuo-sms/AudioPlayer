f0b355fe0cd18d84cba1421947c1bea5d5347601
“Your <yutaro-matsuo@bm-sms.co.jp>
2020-07-03 15:22:42 +0900

    fix AudioPlayerController

--- AudioPlayer_app/app/Http/Controllers/AudioPlayController.php
+++ AudioPlayer_app/app/Http/Controllers/AudioPlayController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AudioPlayController extends Controller
 {
-    public function index()
+    public function __construct()
     {
-        return view('audio_player/index');
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+        /*$user = Auth::user();
+        $sort = $request->sort;
+        $items = User::orderBy($sort, 'asc')
+            ->simplePaginate(5);
+        $param = ['items' => $items, 'sort' => $sort,
+            'user' => $user];*/
+    return view('audio_player/index', /*$param*/);
     }
 }
